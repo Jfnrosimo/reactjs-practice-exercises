8def230578f20f9a309056b39fcf5cac856566f6
jrosi <j.rosimo098@gmail.com>
2022-12-21 20:30:52 +0800

    Feat: added login form component and initial functionalities

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SearchFilter from "./practiceComponents/SearchFilter";
 import Counter from "./practiceComponents/Counter";
 import DisplayList from "./practiceComponents/DisplayList";
 import Slider from "./practiceComponents/slider/Slider";
+import LoginApp from "./practiceComponents/loginForm/LoginApp";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <Counter />
       <DisplayList />
       <Slider />
+      <LoginApp />
     </div>
   );
 };
---
+++ src/practiceComponents/loginForm/LoginApp.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+//Import component
+import LoginForm from "./LoginForm";
+
+const LoginApp = () => {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+  };
+
+  const Logout = (details) => {
+    console.log("Logout");
+  };
+
+  return (
+    <div>
+      {user.email !== "" ? (
+        <div className="welcome">
+          <h2>
+            Welcome, <span>{user.name}</span>
+          </h2>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
+  );
+};
+
+export default LoginApp;
